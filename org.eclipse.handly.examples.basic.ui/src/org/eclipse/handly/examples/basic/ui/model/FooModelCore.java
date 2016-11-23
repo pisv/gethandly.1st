f04ea9988a8ddd7d8dd391a72d6ee747c8c67684
Vladimir Piskarev <pisv@1c.ru>
2016-11-23 13:01:46 +0300

    Migrate to Handly 0.6

--- org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/examples/basic/ui/model/FooModelCore.java
+++ org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/examples/basic/ui/model/FooModelCore.java
@@ -25,7 +25,7 @@ public class FooModelCore
      */
     public static IFooModel getFooModel()
     {
-        return FooModelManager.INSTANCE.getFooModel();
+        return FooModelManager.INSTANCE.getModel();
     }
     
     /**
